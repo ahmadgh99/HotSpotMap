d9116642df0bee22e8595c392a59d46d0833cd89
Raslan <khaled7mar123@gmail.com>
2024-07-23 21:02:25 +0300

    added zebbi and its small currently

--- app/src/main/java/com/example/hotspotmap/MainActivity.kt
+++ app/src/main/java/com/example/hotspotmap/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    private var zebbi: String="small"
+    private var zebbi: String="extra small"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
