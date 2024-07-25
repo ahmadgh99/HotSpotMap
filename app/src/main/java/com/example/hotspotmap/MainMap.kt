08c8d54115b812cc00c8f7a2b450e8ed76d80516
Ahmad Ghanayem <ahmad.g.99@hotmail.com>
2024-07-25 14:07:13 +0300

    Started With MainMap

---
+++ app/src/main/java/com/example/hotspotmap/MainMap.kt
@@ -0,0 +1,11 @@
+package com.example.hotspotmap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_map)
+    }
+}
